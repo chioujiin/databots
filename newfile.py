fd0be256be4da17d54b668c40e95e68da2e7d1fd
Mary <maryhuang@tamu.edu>
2019-09-26 08:31:20 -0500

    added map

--- newfile.py
+++ newfile.py
@@ -26,3 +26,13 @@ finally:
         cursor.close()
         connection.close()
         print("MySQL connection is closed")
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
