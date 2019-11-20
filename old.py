5c9dbd5f6bd49cd7c95c77ef52111046d0dacf13
chioujiin <a109133409@gmail.com>
2019-11-20 08:27:48 -0600

    Add files via upload

--- old.py
+++ old.py
@@ -1,4 +1,6 @@
-import pymongo
+import mysql.connector
+from mysql.connector import Error
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from opencage.geocoder import OpenCageGeocode
 import simplejson as json
@@ -6,32 +8,40 @@ import datetime
 
 app = Flask(__name__)
 
-
-client = pymongo.MongoClient("mongodb+srv://databots:Crazy123@databots-nmkwk.mongodb.net/test?retryWrites=true&w=majority")
-db = client.yelp_new
-collection = db.yelp_business
-
+# when the search route is called by submitting the first button
 @app.route('/search', methods=['GET', 'POST'])
 def getRestaurants():
     try:
+        # we get the city and preference the user entered
         city = request.args.get('search')
         preference=request.args.get('Preference')
+        # we get the date of that request to check if the stores are open that day 
         today=datetime.datetime.now()
         day=today.strftime("%A").lower()
         if city:
-            result = collection.find({"$and":[{'categories': {'$regex':preference}}, {'city':city}]})
-
-            return render_template('search.html', records = result)
+            # if the user enters a city, connect to the database
+            conn = mysql.connector.connect(host='18.221.176.74',
+                                           database='yelp',
+                                           user='databots',
+                                           password='Crazy@123')
+            cursor = conn.cursor()
+            # query to get all the rows that suits the users input
+            # get all the data matching the query
+            cursor.execute("SELECT a.name, a.address, a.city, a.stars, a.review_count, b."+day+" from business a, business_hours b where a.city = %s and a.categories like '%Restaurants%' and a.categories like concat('%',%s,'%') and a.business_id=b.business_id", (city,preference))
+            resultSet = cursor.fetchall()
+            # render the response to search.html, which is our result page 
+            return render_template('search.html', lenrow = len(resultSet), lencol = len(resultSet[0]), records = resultSet)
         else:
-            return None
+            return resultSet
     finally:
-        print('here')
+        if(conn):
+            conn.close()
 
-
-
+# when the destination route is called by submitting the second button
 @app.route('/destination', methods=['GET', 'POST'])
 def searchRestaurants():
     try:
+        # we get the two cities and preference the user entered
         city1 = request.args.get('start')
         city2 = request.args.get('end')
         preference=request.args.get('Preference')
@@ -39,6 +49,12 @@ def searchRestaurants():
         today = datetime.datetime.now()
         day = today.strftime("%A").lower()
         if (city1 and city2):
+            # connect to the database
+            conn = mysql.connector.connect(host='18.221.176.74',
+                                           database='yelp',
+                                           user='databots',
+                                           password='Crazy@123')
+            cursor = conn.cursor()
 
             # the function to get the coordinates for the two cities
             def generate_coordinates(city1, city2):
@@ -76,15 +92,18 @@ def searchRestaurants():
                 longitude_big = longitude2
 
             # we select all restaurant within the regtangle with the preference the user input
-            result = collection.find({"$and":[{'categories': {'$regex':preference}}, {'latitude' : { '$lt' : latitude_big ,"$gt" : latitude_small}}, {"longitude" : { "$lt" : longitude_big ,"$gt" : longitude_small}}]})
-            # result = collection.find({"$and":[{  'categories': {'$regex':preference}}, {"longitude" : { "$lt" : longitude_big ,"$gt" : longitude_small}   }]})
+            cursor.execute("select a.name, a.address, a.city, a.stars, a.review_count, b."+day+" from business a, business_hours b where a.categories like '%Restaurants%' AND a.categories like concat('%',%s,'%') AND a.latitude between %s AND %s AND a.longitude between %s AND %s AND a.business_id=b.business_id",(preference, latitude_small, latitude_big, longitude_small, longitude_big))
+            resultSet = cursor.fetchall()
 
             # render the response to search.html, which is our result page 
-            return render_template('search.html', records = result)
+            return render_template('./search.html', lenrow=len(resultSet), lencol=len(resultSet[0]), records=resultSet)
+        else:
+            return resultSet
     finally:
-        print("done")
-
-
+        if(conn):
+            conn.close()
+    
+# our index route
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
