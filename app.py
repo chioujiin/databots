8691df11782d9b81d291a2c3fd1d2da6653af4fb
chioujiin <a109133409@gmail.com>
2019-12-02 20:51:04 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -3,41 +3,57 @@ from flask import Flask, render_template, jsonify, request, redirect, url_for
 from opencage.geocoder import OpenCageGeocode
 import simplejson as json
 import datetime
-
 app = Flask(__name__)
 
-
+# connect to mongoDB
 client = pymongo.MongoClient("mongodb+srv://databots:Crazy123@databots-nmkwk.mongodb.net/test?retryWrites=true&w=majority")
 db = client.yelp_new
 collection = db.yelp_business
 
+# when the user clicks the search button
 @app.route('/search', methods=['GET', 'POST'])
 def getRestaurants():
     try:
+        # get the information the user entered
         city = request.args.get('search')
-        preference=request.args.get('Preference')
+        stars = int(request.args.get('stars'))
+        preference=request.args.get('Preference2')
         today=datetime.datetime.now()
         day=today.strftime("%A").lower()
+        
+        # if the user enters a city
         if city:
-            result = collection.find({"$and":[{'categories': {'$regex':preference}}, {'city':city}]})
-
-            return render_template('search.html', records = result)
-        else:
-            return None
+            # we get the documents that are as requested by the users
+            result = collection.count_documents({"$and":[{'categories': {'$regex':preference}}, {'city':city}, {'stars' : {"$gte" : stars}}]})
+            
+            # if we didn't get anything, we use the build-in function from pymongo and return a 404 not found response to the front-end
+            if result ==0: 
+                result = collection.find_one_or_404({"$and":[{'categories': {'$regex':preference}}, {'city':city}, {'stars' : {"$gte" : stars}}]})
+                return render_template('search.html', records = result)
+            
+            # if there are results, render the results on the search.html page
+            else: 
+                result = collection.find({"$and":[{'categories': {'$regex':preference}}, {'city':city}, {'stars' : {"$gt" : stars}}]})
+                return render_template('search.html', records = result)
+        else: return None
     finally:
         print('here')
 
 
-
+# when the user clicks the find button
 @app.route('/destination', methods=['GET', 'POST'])
 def searchRestaurants():
     try:
+        # get the information the user entered
         city1 = request.args.get('start')
         city2 = request.args.get('end')
-        preference=request.args.get('Preference')
+        preference=request.args.get('Preference3')
+
         # get the date and get the opening stores
         today = datetime.datetime.now()
         day = today.strftime("%A").lower()
+
+        # if the user enters both cities
         if (city1 and city2):
 
             # the function to get the coordinates for the two cities
@@ -76,7 +92,7 @@ def searchRestaurants():
                 longitude_big = longitude2
 
             # we select all restaurant within the regtangle with the preference the user input
-            result = collection.find({"$and":[{'categories': {'$regex':preference}}, {'latitude' : { '$lt' : latitude_big ,"$gt" : latitude_small}}, {"longitude" : { "$lt" : longitude_big ,"$gt" : longitude_small}}]})
+            result = collection.find({"$and":[{'categories': {'$regex':preference}}, {'latitude' : { '$lt' : latitude_big ,"$gt" : latitude_small}}, {"longitude" : { "$lt" : longitude_big ,"$gt" : longitude_small}}, {'is_open': 1}]})
             # result = collection.find({"$and":[{  'categories': {'$regex':preference}}, {"longitude" : { "$lt" : longitude_big ,"$gt" : longitude_small}   }]})
 
             # render the response to search.html, which is our result page 
@@ -84,7 +100,26 @@ def searchRestaurants():
     finally:
         print("done")
 
+# when the user inserts data
+@app.route('/insert', methods=['GET', 'POST'])
+def insertDocument():
+    try:
+        # get the information the user entered
+        stars = int(request.args.get('star'))
+        restName = request.args.get('restName')
+        city = request.args.get('city')
+        state = request.args.get('state')
+        categories = request.args.get('Preference1')
 
+        # we assume the user will not enter the opening hours now
+        # for implicit schema's sake, we have to set those to None so it will show correctly on the front-end
+        doc = { "name":restName, "city": city,"state":state, "stars":stars,"categories": categories ,"monday":"None", "tuesday": "None", "wednesday": "None","thursday":"None", "friday": "None" ,"saturday":"None", "sunday": "None" }
+        
+        return render_template('index.html') 
+    finally:
+        print("done")
+
+# the index page
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
